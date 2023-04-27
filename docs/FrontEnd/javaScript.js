952b9384a1d3ae109f60ba31911091f993de8efa
julien blondel <julien.blondel218@yahoo.com>
2023-04-27 21:28:29 +0200

    save two 27.04

--- docs/FrontEnd/javaScript.js
+++ docs/FrontEnd/javaScript.js
@@ -1,10 +1,33 @@
 
 
-fetch('http://localhost:5678/api/works')
-    .then(response => (response.json()))
-    .then(response2 => console.table(response2))
 
-fetch('http://localhost:5678/api/categories')
+const works = fetch('http://localhost:5678/api/works')
+    .then(response => (response.json()))
+    .then((data) => {
+        console.log(data);
+        const gallery = document.querySelector(".gallery");
+
+        for (let i = 0; i < 11; i++) {
+            let divGallery = document.createElement("div");
+            gallery.appendChild(divGallery);
+
+
+            for (let galerie of data) {
+                console.log(galerie);
+                divGallery.innerHTML = galerie.title;
+                let pictures = document.createElement("img");
+                divGallery.appendChild(pictures);
+                pictures.src = galerie.imageUrl;
+
+            }
+
+        }
+
+
+    })
+
+
+const categories = fetch('http://localhost:5678/api/categories')
     .then(response => (response.json()))
     .then(response2 => console.table(response2))
 
@@ -22,4 +45,3 @@ for (let i = 0; i < filters.length; i++) {
 
 
 // filtre fonctionnalité
-
